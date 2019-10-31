45ef70770ad288302e86c9e5ef6a937e6e4249af
mel eve <melsie78@hotmail.com>
2019-10-31 10:51:31 +0100

    books schema, books.json and books importer file

---
+++ Book.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// 1. Create a schema for a book
+let bookSchema = new Schema({
+  "author": String,
+  "country": String,
+  "image": String,
+  "language": String,
+  "link": String,
+  "pages": Number,
+  "title": String,
+  "year": Number
+});
+
+// 2. Create a class with methods/or getters/setters
+//    that every books should have
+
+class BookClass {
+
+  get age() {
+    let currentYear = new Date().getFullYear();
+    return currentYear - this.year;
+  }
+
+}
+
+// 3. Create the model and export it
+bookSchema.loadClass(BookClass);
+module.exports = db.model('Book', bookSchema);
